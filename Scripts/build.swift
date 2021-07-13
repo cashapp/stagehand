8ee909ffc477a10f36363ed0eb0ed1890606f50c
Nick Entin <entin@squareup.com>
2021-07-12 20:47:04 -0700

    Add iOS 14 to matrix

--- Scripts/build.swift
+++ Scripts/build.swift
@@ -46,11 +46,14 @@ enum TaskError: Error {
 }
 
 enum Platform: String, CustomStringConvertible {
+	case iOS_14
 	case iOS_13
 	case iOS_12
 
 	var destination: String {
 		switch self {
+		case .iOS_14:
+			return "platform=iOS Simulator,OS=14.4,name=iPhone 12 Pro"
 		case .iOS_13:
 			return "platform=iOS Simulator,OS=13.3,name=iPhone 11 Pro"
 		case .iOS_12:
